def8aa1fcb9d57f16078634ba286546a062c58ec
Riii40 <62451491+FullowID@users.noreply.github.com>
2022-03-18 15:22:15 +0800

    Add files via upload

---
+++ modul1/PRAK101.kt
@@ -0,0 +1,14 @@
+package modul1
+
+fun main(){
+    print("Waktu Sekarang: ")
+    val waktu = readLine()
+    print("Nama Anda: ")
+    val nama = readLine()
+    print("Umur Anda: ")
+    val umur = readLine()
+    print("Suhu Tubuh Anda: ")
+    val suhu = readLine()
+    println("Selamat Pagi, ${nama}. Umur anda ${umur} Tahun.")
+    println("Suhu Tubuh anda ${suhu} derajat Celcius")
+}
